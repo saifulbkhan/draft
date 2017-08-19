4d57180b6e52e35488c8ea5e6eb6910e0cf0cf19
Saiful B. Khan <saifulbkhan@gmail.com>
2017-08-19 21:13:33 +0530

    Add proper application window creation and handling
    Also remove a swap file for vim that crept in from initial commit.

--- notosrc/app.py
+++ notosrc/app.py
@@ -1,15 +1,59 @@
 import sys
 import gi
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+
+gi.require_version('Notify', '0.7')
+from gi.repository import Gtk, GLib, Gio, Notify
 
 class Application(Gtk.Application):
-    def __init__(self, **kwargs):
-        super().__init__(application_id='org.gnome.Noto',
-                         **kwargs)
+    def __repr__(self):
+        return '<Application>'
+
+    def __init__(self):
+        Gtk.Application.__init__(self, application_id='org.gnome.Noto',
+                                 flags=Gio.ApplicationFlags.FLAGS_NONE)
+        GLib.set_application_name("Noto")
+        GLib.set_prgname('noto')
+        self._window = None
+
+    def _build_app_menu(self):
+        action_entries = [
+            ('about', self._about),
+            ('quit', self.quit),
+        ]
+
+        for action, callback in action_entries:
+            simple_action = Gio.SimpleAction.new(action, None)
+            simple_action.connect('activate', callback)
+
+    def _about(self):
+
+        def about_response(dialog, response):
+            dialog.destroy()
+
+        about = Gtk.AboutDialog()
+        about.set_transient_for(self._window)
+        about.connect("response", about_response)
+        about.show()
+
+    def quit(self):
+        self._window.destroy()
+
+    def do_startup(self):
+        Gtk.Application.do_startup(self)
+        Notify.init("Noto")
+        self._build_app_menu()
 
     def do_activate(self):
-        window = Gtk.ApplicationWindow.new(self)
-        window.set_default_size(200, 200)
-        window.set_title('Noto')
-        window.present()
+        if not self._window:
+            self._window = ApplicationWindow(self)
+        self._window.present()
+
+class ApplicationWindow(Gtk.ApplicationWindow):
+    def __repr__(self):
+        return '<ApplicationWindow>'
+
+    def __init__(self, app):
+        Gtk.ApplicationWindow.__init__(self,
+                                       application=app,
+                                       title="Noto")
+        self.set_size_request(950, 600) 
