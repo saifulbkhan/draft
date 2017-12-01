a4bb4d0dea63031caba91458ce26d42ec6eac019
Saiful B. Khan <saifulbkhan@gmail.com>
2017-12-02 02:16:32 +0530

    Create specialized class for text editor
    GtkSource provides a good text editing framework by itself but it
    does not provide all the utility that Noto would like to have. A
    new subclass 'NotoTextView' will be extending GtkSourceView to have
    features like scroll offset and centered cursor mode among others.

--- notosrc/textview.py
+++ notosrc/textview.py
@@ -19,12 +19,13 @@ from gettext import gettext as _
 import gi
 gi.require_version('GtkSource', '3.0')
 
-from gi.repository import Gtk, GObject, GtkSource, Gdk, GLib
+from gi.repository import Gtk, GObject, GtkSource, Gdk, GLib, Pango
 
 from notosrc import file
 
 # Ensure that GtkBuilder actually recognises SourceView in UI file
 GObject.type_ensure(GObject.GType(GtkSource.View))
+
 
 class TextView(Gtk.Box):
     def __repr__(self):
@@ -36,17 +37,27 @@ class TextView(Gtk.Box):
         self.builder.add_from_resource('/org/gnome/Noto/textview.ui')
         self.main_window = main_window
         self.parent = parent
-        self.view = self.builder.get_object('editor')
         self._set_up_widgets()
         self.current_file = None
         self.current_file_etag = None
 
     def _set_up_widgets(self):
-        # Use OrderedDict so that packing occurs in order of declaration
-        widgets = OrderedDict({
-            'scrollable_editor': (True, True, 0),
-            'status_bar': (False, False, 0)
-        })
+        self.scrollable = self.builder.get_object('scrollable')
+        self.pack_start(self.scrollable, True, True, 0)
+        self.status_bar = self.builder.get_object('status_bar')
+        self.pack_start(self.status_bar, False, False, 0)
+        self.view = NotoTextView()
+        self.scrollable.add(self.view)
+
+        self.view.set_pixels_above_lines(6)
+        self.view.set_pixels_below_lines(6)
+        self.view.set_wrap_mode(Gtk.WrapMode.WORD)
+        self.view.set_left_margin(24)
+        self.view.set_right_margin(24)
+        self.view.set_top_margin(10)
+        self.view.set_bottom_margin(10)
+        self.view.scroll_offset = 10
+
         self.connect('key-press-event', self._on_key_press)
 
         buffer = self.view.get_buffer()
@@ -55,14 +66,6 @@ class TextView(Gtk.Box):
         language_manager = GtkSource.LanguageManager.get_default()
         language = language_manager.get_language('markdown')
         buffer.set_language(language)
-
-        self._generate_text_view(widgets)
-
-    def _generate_text_view(self, widgets):
-        for widget_name, pack_info in widgets.items():
-            widget = self.builder.get_object(widget_name)
-            expand, fill, padding = pack_info
-            self.pack_start(widget, expand, fill, padding)
 
     def _on_key_press(self, widget, event):
         modifiers = Gtk.accelerator_get_default_mod_mask()
@@ -99,7 +102,7 @@ class TextView(Gtk.Box):
         iter_at_cursor = buffer.get_iter_at_mark(buffer.get_insert())
         GLib.idle_add(self.view.scroll_to_iter,
                       iter_at_cursor,
-                      0.0, False, 0.0, 0.0)
+                      0.0, True, 0.0, 0.5)
 
     def write_current_buffer(self):
         buffer = self.view.get_buffer()
@@ -135,3 +138,142 @@ class TextView(Gtk.Box):
             return self._get_title_for_text(title)
 
         return title
+
+
+class NotoTextView(GtkSource.View):
+    __gtype_name__ = 'NotoTextView'
+
+    def __repr__(self):
+        return '<NotoTextView>'
+
+    def init(self):
+        GtkSource.View.__init__(self)
+        self.cached_char_height = 0
+        self.cached_char_width = 0
+        self.scroll_offset = 0
+
+    def do_style_updated(self):
+        GtkSource.View.do_style_updated(self)
+
+        context = self.get_pango_context()
+        layout = Pango.Layout(context)
+        layout.set_text('X', 1)
+        self.cached_char_width, self.cached_char_height = layout.get_pixel_size()
+
+    def get_visible_rect(self):
+        area = GtkSource.View.get_visible_rect(self)
+
+        # If we don't have valid line height, not much we can do now. We can
+        # just adjust things later once it becomes available.
+        if self.cached_char_height:
+            visible_lines = int(area.height / self.cached_char_height)
+            max_scroll_offset = (visible_lines - 1)/ 2
+            scroll_offset = min(self.scroll_offset, max_scroll_offset)
+            scroll_offset_height = self.cached_char_height * scroll_offset
+
+            area.y += scroll_offset_height
+            area.height -= (2* scroll_offset_height)
+
+            # If we have an even number of visible lines and scrolloffset is
+            # less than our desired scrolloffset, we need to remove an extra
+            # line so we don't have two visible lines.
+            if scroll_offset < self.scroll_offset and (visible_lines & 1) == 0:
+                area.height -= self.cached_char_height
+
+            # Use a multiple of the line height so we don't jump around when
+            # focusing the last line (due to Y2 not fitting in the visible
+            # area).
+            area.height = (area.height / self.cached_char_height) \
+                          * self.cached_char_height
+
+        return area
+
+    def scroll_to_iter(self, text_iter, within_margin,
+                       use_align, xalign, yalign):
+        buffer = self.get_buffer()
+
+        hadj = self.get_hadjustment()
+        vadj = self.get_vadjustment()
+
+        rect = self.get_iter_location(text_iter)
+        screen = self.get_visible_rect()
+
+        current_x_scroll = screen.x
+        current_y_scroll = screen.y
+
+        screen_x_offset = screen.width * within_margin
+        screen_y_offset = screen.height * within_margin
+
+        screen.x += screen_x_offset
+        screen.y = screen_y_offset
+        screen.width -= screen_x_offset * 2
+        screen.height -= screen_y_offset * 2
+
+        if screen.width < 1:
+            screen.width = 1
+        if screen.height < 1:
+            screen.height = 1
+
+        # The -1 here ensures that we leave enough space to draw the cursor
+        # when this function is used for horizontal scrolling.
+        screen_right = screen.x + screen.width - 1
+        screen_bottom = screen.y + screen.height
+
+        # The alignment affects the point in the target character that we
+        # choose to align. If we're doing right/bottom alignment, we align
+        # the right/bottom edge of the character the mark is at; if we're
+        # doing left/top we align the left/top edge of the character; if
+        # we're doing center alignment we align the center of the
+        # character.
+
+        # Vertical alignment
+        scroll_dest = current_y_scroll
+        yvalue = 0
+        if use_align:
+            scroll_dest = rect.y + (rect.height * yalign) - (screen.height * yalign);
+
+            # if scroll_dest < screen.y, we move a negative increment (up),
+            # else a positive increment (down)
+            yvalue = scroll_dest - screen.y + screen_y_offset
+        else:
+            if rect.y < screen.y:
+                scroll_dest = rect.y
+                yvalue = scroll_dest - screen.y - screen_y_offset
+            elif (rect.y + rect.height) > screen_bottom:
+                scroll_dest = rect.y + rect.height
+                yvalue = scroll_dest - screen_bottom + screen_y_offset
+        yvalue += current_y_scroll
+
+        # Scroll offset adjustment
+        if self.cached_char_height:
+            visible_lines = int(screen.height / self.cached_char_height)
+            max_scroll_offset = (visible_lines - 1)/ 2
+            scroll_offset = min(self.scroll_offset, max_scroll_offset)
+            scroll_offset_height = self.cached_char_height * scroll_offset
+
+            if scroll_offset_height > 0:
+                if rect.height - scroll_offset_height < yvalue:
+                    yvalue -= (scroll_offset_height - (rect.y - yvalue))
+                elif (rect.y + rect.height) + scroll_offset_height > yvalue + screen.height:
+                    yvalue += (rect.y + rect.height + scroll_offset_height) - (yvalue + screen.height)
+
+        # Horizontal alignment
+        scroll_dest = current_x_scroll
+        xvalue = 0
+        if use_align:
+            scroll_dest = rect.x + (rect.width * xalign) - (screen.width * xalign)
+
+            # if scroll_dest < screen.y, we move a negative increment (left),
+            # else a positive increment (right)
+            xvalue = scroll_dest - screen.x + screen_x_offset
+        else:
+            if rect.x < screen.x:
+                scroll_dest = rect.x
+                xvalue = scroll_dest - screen.x - screen_x_offset
+            elif (rect.x + rect.width) > screen_right:
+                scroll_dest = rect.x + rect.width
+                xvalue = scroll_dest - screen_right + screen_x_offset
+        xvalue += current_x_scroll
+
+        hadj.set_value(xvalue)
+        vadj.set_value(yvalue)
