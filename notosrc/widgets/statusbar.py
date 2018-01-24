fb9010a28fce452327453a36181bcd505a7c2549
Saiful B. Khan <saifulbkhan@gmail.com>
2018-01-24 19:59:24 +0530

    Introduce word goals and remove markup and overwrite mode labels
    The markup and overwrite mode indicators are disctracting. Markup
    can be checked and set (for later sessions) from a separate section
    meant for document preferences. Overwrite mode is indicated by the
    editor itself (fat cursor bar). Word count is still useful and
    something that the writer would like to keep an eye on.
    
    The UI for setting word goals has been implemented as well, but
    is quite useless since it needs to be preserved over sessions.

--- notosrc/widgets/statusbar.py
+++ notosrc/widgets/statusbar.py
@@ -29,7 +29,7 @@ def registered_for_update(fn):
     return fn
 
 
-class NotoStatusbar(Gtk.Box):
+class NotoStatusbar(Gtk.Bin):
     """A status bar for providing tagging and some mimimal information"""
 
     __gtype_name__ = 'NotoStatusbar'
@@ -43,7 +43,7 @@ class NotoStatusbar(Gtk.Box):
         @self: NotoStatusbar
         @editor: NotoEditor, the editor which @self serves
         """
-        Gtk.Box.__init__(self)
+        Gtk.Bin.__init__(self)
         self.editor = editor
         self.builder = Gtk.Builder()
         self.builder.add_from_resource('/org/gnome/Noto/statusbar.ui')
@@ -52,20 +52,22 @@ class NotoStatusbar(Gtk.Box):
 
     def _set_up_widgets(self):
         """Set up widgets contained within statusbar"""
-        self.overwrite_mode_label = Gtk.Label()
-        self.overwrite_mode_label.get_style_context()\
-                                 .add_class('noto-statusbar-widget')
-        self.pack_end(self.overwrite_mode_label, False, False, 0)
+        self.main_button_box = self.builder.get_object('main_button_box')
+        self.add(self.main_button_box)
 
-        self.word_count_label = Gtk.Label()
-        self.word_count_label.get_style_context()\
-                             .add_class('noto-statusbar-widget')
-        self.pack_end(self.word_count_label, False, False, 0)
+        self.word_count_label = self.builder.get_object('word_count_label')
+        self.word_goal_label = self.builder.get_object('word_goal_label')
+        self.word_goal_popover = self.builder.get_object('word_goal_popover')
+        self.goal_set_entry = self.builder.get_object('word_goal_entry')
+        word_goal_button = self.builder.get_object('word_goal_button')
+        stack = self.builder.get_object('word_goal_stack')
+        goal_edit_button = self.builder.get_object('word_goal_edit_button')
+        goal_set_button = self.builder.get_object('word_goal_set_button')
 
-        self.markup_label = Gtk.Label()
-        self.markup_label.get_style_context()\
-                         .add_class('noto-statusbar-widget')
-        self.pack_end(self.markup_label, False, False, 0)
+        self.goal_set_entry.connect('activate', self._on_set_goal, stack)
+        word_goal_button.connect('clicked', self._on_word_count_clicked)
+        goal_edit_button.connect('clicked', self._on_request_goal_change, stack)
+        goal_set_button.connect('clicked', self._on_set_goal, stack)
 
         self.tag_labels = self.builder.get_object('tag_labels')
         self.tag_popover = self.builder.get_object('tag_popover')
@@ -78,8 +80,6 @@ class NotoStatusbar(Gtk.Box):
         new_tag_button.connect('clicked', self._on_tag_added)
         clickable.connect('clicked', self._on_labels_clicked)
 
-        self.pack_start(clickable, True, True, 0)
-
     def update_state(self):
         """Update the state of statusbar to represent the currently visible
         text-view in parent NotoEditor.
@@ -87,7 +87,6 @@ class NotoStatusbar(Gtk.Box):
         for update_method in UPDATE_REGISTRY:
             update_method(self)
 
-    @registered_for_update
     def update_overwrite_mode(self):
         """Update @self::overwrite_mode_label to denote whether editor is in overwrite
         mode or not.
@@ -108,7 +107,6 @@ class NotoStatusbar(Gtk.Box):
         num_words = len(word_rule.findall(text))
         self.word_count_label.set_label(str(num_words) + ' ' + _("Words"))
 
-    @registered_for_update
     def update_markup(self):
         """Update @self::markup_label to the type of markup being used in
         text view"""
@@ -184,7 +182,7 @@ class NotoStatusbar(Gtk.Box):
             _append_to_list(tag, 'window-close-symbolic', self._on_tag_deleted)
 
     def _on_labels_clicked(self, button, user_data=None):
-        """Handle click on labels"""
+        """Handle click on tag labels"""
         self.tag_popover.popup()
         self.new_tag_entry.grab_focus()
 
@@ -199,3 +197,26 @@ class NotoStatusbar(Gtk.Box):
         """Handle deletion of a tag from the note"""
         tag = label.get_label()
         self.editor.delete_tag(tag)
+
+    def _on_word_count_clicked(self, widget, user_data=None):
+        """Handle click on word count label"""
+        self.word_goal_popover.popup()
+
+    def _switch_stack_child(self, stack):
+        """Given a GtkStack with two children switch to the other child"""
+        children = stack.get_children()
+        child1 = children[0]
+        child2 = children[1]
+        if stack.get_visible_child() is child1:
+            stack.set_visible_child(child2)
+        else:
+            stack.set_visible_child(child1)
+
+    def _on_request_goal_change(self, widget, stack=None):
+        self._switch_stack_child(stack)
+        self.goal_set_entry.grab_focus()
+
+    def _on_set_goal(self, widget, stack=None):
+        goal = self.goal_set_entry.get_text()
+        self.word_goal_label.set_markup('<span size="x-large">%s</span>' % goal)
+        self._switch_stack_child(stack)
